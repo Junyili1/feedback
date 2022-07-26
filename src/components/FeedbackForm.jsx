cfcef465cd865c8f93c537dbdc42459fb3513489
Apples <junyiappleli@gmail.com>
2022-07-26 14:22:18 -0400

    added rating

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,11 +1,26 @@
 import { useState } from "react";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+  const [rating, setRating] = useState(10);
 
   const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if ((text !== "") & (text.trim().length <= 10)) {
+      setBtnDisabled(true);
+      setMessage("Text must be greater than 10 characters");
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+
     setText(e.target.value);
   };
 
@@ -13,7 +28,7 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would you rate our service?</h2>
-        {/* @todo - rating select component*/}
+        <RatingSelect />
         <div className="input-group">
           <input
             onChange={handleTextChange}
@@ -21,8 +36,11 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div classname="message">{message}</div>}
       </form>
     </Card>
   );
