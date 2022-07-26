fc160846e419da60f5e93c232dede9becfae3dd1
Apples <junyiappleli@gmail.com>
2022-07-26 13:56:08 -0400

    added data and form

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,22 +1,21 @@
-import { useState } from "react";
+import { FaTimes } from "react-icons/fa";
+import Card from "./shared/Card";
+import PropTypes from "prop-types";
 
-function FeedbackItem() {
-  const [rating, setRating] = useState(7);
-  const [text, setText] = useState("Default Text");
-
-  const handleClick = () => {
-    setRating((prev) => {
-      return prev++;
-    });
-  };
-
+function FeedbackItem({ item, handleDelete }) {
   return (
-    <div className="card">
-      <div className="num-display">{rating}</div>
-      <div className="text-display">{text}</div>
-      <button onClick={handleClick}>Click</button>
-    </div>
+    <Card>
+      <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className="close">
+        <FaTimes color="purple" />
+      </button>
+      <div className="text-display">{item.text}</div>
+    </Card>
   );
 }
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+};
 
 export default FeedbackItem;
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,5 +1,28 @@
-function FeedbackList({ feedback }) {
-  return <div>List</div>;
+import FeedbackItem from "./FeedbackItem";
+import PropTypes from "prop-types";
+
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>There are no feedback here!</p>;
+  }
+
+  return (
+    <div className="feedback-list">
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+      ))}
+    </div>
+  );
 }
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+    })
+  ),
+};
 
 export default FeedbackList;
