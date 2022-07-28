2713fce0094fc2c884ad618babdf372052bff39b
Apples <junyiappleli@gmail.com>
2022-07-28 13:13:59 -0400

    rating system

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -24,11 +24,22 @@ function FeedbackForm() {
     setText(e.target.value);
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+      console.log(newFeedback);
+    }
+  };
+
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate our service?</h2>
-        <RatingSelect />
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
             onChange={handleTextChange}
@@ -40,7 +51,7 @@ function FeedbackForm() {
             Send
           </Button>
         </div>
-        {message && <div classname="message">{message}</div>}
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
